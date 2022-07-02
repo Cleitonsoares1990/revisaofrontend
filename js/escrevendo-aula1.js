4af8e05ef651345eaa969f970f6fe082a64707c6
Cleiton <cleitonsoares1990@gmail.com>
2022-07-01 21:52:42 -0300

    inicio

--- js/escrevendo-aula1.js
+++ js/escrevendo-aula1.js
@@ -10,8 +10,11 @@ var button = document.querySelector("button");
 
 button.onclick = function () {
   var nome = prompt("Qual é o comando para iniciar o git?");
-  if ((nome = "git init")) {
-    alert("O" + nome + ", serve para iniciar o git.Parabéns você acertou!");
+  if ((nome == "git init")) {
+    alert("O" +  nome  + " , serve para iniciar o git.Parabéns você acertou!");
+  }
+  else {
+    alert(  "  Resposta Errada! ");
   }
   window.location="aula2.html";
 };
